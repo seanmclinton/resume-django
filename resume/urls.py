e29f305022542bdcdf4a1f386e427cefba195521
seanmclinton <sean96@vt.edu>
2021-08-18 19:47:02 -0400

    add views for tech used, expanding available end points for each resource type

--- resume/urls.py
+++ resume/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('resume/', views.resume_list, name="resume_list"),
     path('resume/<int:pk>/', views.resume_detail, name="resume_detail"),
     path('contact_info/', views.contact_list, name="contact_list"),
+    path('contact_info/<int:pk>/', views.contact_detail, name="contact_detail"),
+    path('tech_used/', views.techology_used_list, name="tech_used_list"),
+    path('tech_used/<int:pk>/', views.technology_used_detail, name="tech_used_detail"),
+
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.core import exceptions
 from rest_framework import status
 
 from .serializers import *
@@ -19,8 +20,15 @@ def resume_list(request):
 
 
 @csrf_exempt
-def resume_detail(request):
-    pass
+def resume_detail(request, pk):
+    if request.method == "GET":
+        try:
+            resumes = Resume.objects.get(id=pk)
+            serializer = ResumeSerializer(resumes, many=False)
+            return JsonResponse(serializer.data, safe=False)
+        except exceptions.ObjectDoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
 
 @csrf_exempt
 def contact_list(request):
@@ -31,8 +39,30 @@ def contact_list(request):
 
 
 @csrf_exempt
+def contact_detail(request, pk):
+    if request.method == "GET":
+        try:
+            contact = ContactInfo.objects.get(id=pk)
+            serializer = ContactInfoSerializer(contact, many=False)
+            return JsonResponse(serializer.data, safe=False)
+        except exceptions.ObjectDoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+
+@csrf_exempt
 def techology_used_list(request):
     if request.method == "GET":
         tech = TechnologyUsed.objects.all()
         serializer = TechnologyUsedSerializer(tech, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+
+@csrf_exempt
+def technology_used_detail(request, pk):
+    if request.method == "GET":
+        try:
+            tech_used = TechnologyUsed.objects.get(id=pk)
+            serializer = TechnologyUsedSerializer(tech_used, many=False)
+            return JsonResponse(serializer.data, safe=False)
+        except exceptions.ObjectDoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
