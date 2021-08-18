f2eb3b98aa5467217c6568c15067b2529abab73a
seanmclinton <sean96@vt.edu>
2021-08-18 15:52:52 -0400

    removed unused line

--- resume/urls.py
+++ resume/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('resume/', views.resume_list),
     path('resume/<int:pk>/', views.resume_detail),
     path('contact_info/', views.contact_list),
-    # path('snippets/<int:pk>/', views.snippet_detail),
 ]
