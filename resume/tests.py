9238289210d43d953c92bbc5376bcd456d80f9e6
seanmclinton <sean96@vt.edu>
2021-08-17 21:41:23 -0400

    Creating Basic Test Cases

--- resume/tests.py
+++ resume/tests.py
@@ -3,13 +3,19 @@ from resume.models import Resume, ContactInfo
 import datetime
 # Create your tests here.
 
+
 class ResumeTestCase(TestCase):
     def setUp(self) -> None:
-        job_a_start_date = datetime.datetime(2020, 2, 4)
-        job_a_end_date = datetime.datetime(2021, 1, 5)
+        job_a_start_date = datetime.datetime(2020, 2, 4, 0, 0)
+        job_a_end_date = datetime.datetime(2021, 1, 5, 0, 0)
         Resume.objects.create(job_title="Test Job A", company="Test Company A", description="Test Description A",
                               start_date=job_a_start_date, end_date=job_a_end_date)
 
     def test_resume_created(self):
         test_object = Resume.objects.get(job_title="Test Job A")
         self.assertEqual(test_object.description, "Test Description A")
+        self.assertEqual(test_object.company, "Test Company A")
+
+
+class ContactInfoTestCase(TestCase):
+    pass
